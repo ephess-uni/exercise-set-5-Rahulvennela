529c3c3f903540b51dd5f03f77b0dd141a4f3f30
Rahulvennela <128725670+Rahulvennela@users.noreply.github.com>
2023-04-22 21:41:48 +0530

    Update ex_5_1.py

--- src/ex_5_1.py
+++ src/ex_5_1.py
@@ -10,11 +10,9 @@ def main(infile):
     line_count(infile)
 
 
-if __name__ == "__main__":
-    # Create your argument parser object here.
-    # Collect the filename argument from the command line
-    # pass this argument to the main function above
-    # Tests will run your command using a system call.
-    # To test your program with arguments, run it from the command line
-    # (see README.md for more details)
-    pass
+if __name__=='__main__':
+    parser=argparse.ArgumentParser(
+    description='ArgumentParser OBjecct')
+    parser.add_argument('infile',help='positional argument--> INFILE')
+    args=parser.parse_args()
+    btf.line_count(args.infile)
