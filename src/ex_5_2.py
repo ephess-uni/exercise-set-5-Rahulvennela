ba1e9c1d43b6c4c6034c82b11953c39e6d9e9dfd
Rahulvennela <128725670+Rahulvennela@users.noreply.github.com>
2023-04-22 21:51:37 +0530

    Update ex_5_2.py

--- src/ex_5_2.py
+++ src/ex_5_2.py
@@ -13,18 +13,21 @@ try:
 except ImportError:
     from util import get_repository_root
 
-
 if __name__ == "__main__":
 
     # Use these predefined input / output files
     root_dir = get_repository_root()
     INFILE = root_dir / "data" / "ex_5_2-data.csv"
     OUTFILE = root_dir / "outputs" / "ex_5_2-processed.csv"
-    raw_data = np.loadtxt(INFILE, delimiter=',')
-    raw_data-=raw_data.mean()
-    nana=raw_data.std()
-    processed=raw_data/nana
+
+    # Complete the data processing steps using numpy here.
+    data = np.loadtxt(INFILE, delimiter=',')
+    data -= np.mean(data)
+    data /= np.std(data)
+    processed = data
+    # Save the output to OUTFILE using numpy routines.
     np.savetxt(OUTFILE, processed, delimiter=',')
+
 
 
     # Complete the data processing steps using numpy here.
